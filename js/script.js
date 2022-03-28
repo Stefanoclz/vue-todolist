57e4906fbb0eb060388554e2c5872d500a8284eb
Stefano Calzoni <stefanoclz@hotmail.it>
2022-03-28 15:03:49 +0200

    stampato l'array nell'html e aggiunto css

--- js/script.js
+++ js/script.js
@@ -2,5 +2,32 @@ console.log('JS OK');
 
 const app = new Vue({
     el: '#root',
+    data: {
+        list: [
+            {
+                text: 'Fare il caffè',
+                done: false,
+            },
+
+            {
+                text: 'rifare il letto',
+                done: false,
+            },
+
+            {
+                text: 'lavare i pavimenti',
+                done: false,
+            },
+
+            {
+                text: 'nutrire il gatto',
+                done: false,
+            }
+        ],
+
+        methods: {
+
+        }
+    }
 
 })
